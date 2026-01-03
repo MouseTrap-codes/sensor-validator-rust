bac2acbf947ea3a6cbfe826134ecc8c84247f9fe
MouseTrap-codes <mousetrapcodes@gmail.com>
2026-01-03 16:37:19 -0600

    wrote fn for reading csv data with serde

--- src/read_csv.rs
+++ src/read_csv.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, fs::File};
+
 use crate::sensor::{Sensor, SensorType};
 
 #[derive(Debug, serde::Deserialize)]
@@ -5,5 +7,21 @@ pub struct DataLine {
     pub sensor_type: String,
     pub value: f64,
     pub timestamp: i32,
+}
+
+fn read_data_lines(file: File) -> Result<Vec<Sensor>, Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_reader(file);
+    let mut sensors = Vec::new();
+
+    for result in rdr.deserialize() {
+        let record: DataLine = result?;
+
+        match Sensor::try_from(record) {
+            Ok(sensor) => sensors.push(sensor),
+            Err(e) => eprintln!("Skipping invalid record: {}", e),
+        };
+    }
+
+    Ok(sensors)
 }
 
